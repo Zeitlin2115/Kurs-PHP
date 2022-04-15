92e170eef741361eb443a30545747d7f88e96417
Zeitlin2115 <103568419+Zeitlin2115@users.noreply.github.com>
2022-04-15 08:40:52 +0200

    Update zaloguj.php

--- php2/zaloguj.php
+++ php2/zaloguj.php
@@ -1,7 +1,7 @@
 <?php
 	session_start();
 
-	if(!isset($_POST['login'])) || (!isset($_POST['haslo'])))
+	if((!isset($_POST['login'])) || (!isset($_POST['haslo'])))
 	{
 		header('Location: index.php');
 		exit();
